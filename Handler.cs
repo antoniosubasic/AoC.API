773605f298c67c027bed4b5e5ee0084dafb3c6a3
hckmtrx <antonio.subasic.public@gmail.com>
2023-07-29 18:46:05 +0200

    Add files via upload

---
+++ Handler.cs
@@ -0,0 +1,41 @@
+﻿namespace AoC.API;
+
+public class APIHandler
+{
+    const string DOMAIN = "https://adventofcode.com";
+    private string session { get; set; }
+    public APIHandler(string session)
+    {
+        this.session = session;
+    }
+
+
+    private string Request(string url)
+    {
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Cookie", $"session={session}");
+
+        return client.GetStringAsync(url).Result;
+    }
+
+    public string GetInput(int year, int day) => Request($"{DOMAIN}/{year}/day/{day}/input");
+
+    public Dictionary<int, int> GetStars()
+    {
+        var response = Request($"{DOMAIN}/events")
+            .Split('\n')
+            .Where(line => line.StartsWith("<div class=\"eventlist-event\">"))
+            .ToArray();
+
+        return response.Select(line =>
+        {
+            var yearIndex = line.IndexOf("</a>") - 5;
+            var starIndex = line.IndexOf("</span>") - 3;
+
+            var year = int.Parse(line.Substring(yearIndex, 4));
+            var stars = starIndex < 0 ? 0 : int.Parse(line.Substring(starIndex, 2));
+
+            return new { Year = year, Stars = stars };
+        }).ToDictionary(x => x.Year, x => x.Stars);
+    }
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+<!-- [![Version](https://img.shields.io/nuget/v/AoCAPI)](https://www.nuget.org/packages/AoCAPI) -->
+
+A simple [NuGet](https://nuget.org) package to handle personal [Advent Of Code](https://adventofcode.com) data
+
+## Documentation
+- [Variable Initialization](#variable-initialization)
+- [Features](#features)
+
+<br><br>
+
+# Variable Initialization
+```csharp
+var client = new APIHandler("session cookie");
+```
+> [How to obtain session cookie](https://mmhaskell.com/blog/2023/1/30/advent-of-code-fetching-puzzle-input-using-the-api#authentication)
+
+<br>
+
+# Features
+
+
+## Fetch input files
+
+### Method parameters and return value
+```csharp
+string GetInput(int year, int day)
+```
+
+### Usage
+```csharp
+string input = client.GetInput(2015, 1); // input file from year 2015, day 1
+```
+
+<br>
+
+## Fetch achieved stars
+
+### Method parameters and return value
+```csharp
+Dictionary<int, int> GetStars()
+```
+
+### Usage
+```csharp
+Dictionary<int, int> stars = client.GetStars(); // user's achieved stars by year
+```
